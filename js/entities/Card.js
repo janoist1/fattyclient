073c50b34daed6ce1d6165a26d486fa838c02f6b
Istvan Jano <janoist1@gmail.com>
2014-05-12 14:44:27 +0200

    Implemented functions: fromId() and getId()

--- js/entities/Card.js
+++ js/entities/Card.js
@@ -9,7 +9,17 @@ function Card(type, value) {
     this.type = type || null;
     this.value = value || null;
 
-    this.toString = function () {
+    this.getId = function () {
         return this.type + "_" + this.value;
+    }
+
+    this.fromId = function (id) {
+        var card = id.split('_');
+        this.type = card[0];
+        this.value = card[1];
+    }
+
+    this.toString = function () {
+        return this.getId();
     }
 }
