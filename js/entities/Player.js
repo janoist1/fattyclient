90b937cc375daf7dafb43757c68a5366ebbfa2bf
Istvan Jano <janoist1@gmail.com>
2014-05-28 16:51:23 +0200

    Added property isActive

--- js/entities/Player.js
+++ js/entities/Player.js
@@ -3,13 +3,15 @@
  * @param id
  * @param name
  * @param isReady
+ * @param isActive
  * @constructor
  */
-function Player(id, name, isReady) {
+function Player(id, name, isReady, isActive) {
 
     this.id = id || null;
     this.name = name || null;
     this.isReady = isReady || false;
+    this.isActive = isActive || false;
     this.cardsHand = new CardSet();
     this.cardsUp = new CardSet();
     this.cardsDown = new CardSet();
