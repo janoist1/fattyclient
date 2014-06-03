e707686f15640ca633646f62b143e725c7b36c1a
Istvan Jano <janoist1@gmail.com>
2014-06-03 10:39:13 +0200

    Highlight card in Hand

--- js/graphics/Hand.js
+++ js/graphics/Hand.js
@@ -13,6 +13,7 @@
 
         this.cardSet = cardSet;
         this.cards = [];
+        this.highlights = [];
         this.shadow = new createjs.Shadow("rgba(0,0,0,0.25)", 5, 5, 15);
     };
 
@@ -29,9 +30,10 @@
             var rotation = range / -2 + (range / (Math.max(1, length - 1)) * i);
             var rad = rotation * Math.PI / 180;
             var h = height * 2;
+            var highlight = this.isHighlight(this.cardSet.cards[i]) ? 1.05 : 1;
 
-            card.x = width / 2 + Math.sin(rad) * ((width / 2 + graphics.Card.WIDTH / 2));
-            card.y = h - Math.cos(rad) * ((h - graphics.Card.HEIGHT));
+            card.x = width / 2 + Math.sin(rad) * ((width / 2 + graphics.Card.WIDTH / 2)) * highlight;
+            card.y = h - Math.cos(rad) * ((h - graphics.Card.HEIGHT)) * highlight;
             card.regX = graphics.Card.WIDTH;
             card.regY = graphics.Card.HEIGHT;
             card.rotation = rotation;
@@ -40,6 +42,20 @@
             this.addChild(card);
         }
     }
+
+    Hand.prototype.highlight = function (card) {
+        if (!this.isHighlight(card)) {
+            this.highlights.push(card);
+        }
+    };
+
+    Hand.prototype.unHighlight = function (card) {
+        this.highlights = _.without(this.highlights, card);
+    };
+
+    Hand.prototype.isHighlight = function (card) {
+        return _.contains(this.highlights, card);
+    };
 
     graphics.Hand = Hand;
 
