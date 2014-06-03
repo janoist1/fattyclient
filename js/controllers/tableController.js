a553fec2dc0cd504c43f3be60c747f857188ccf8
Istvan Jano <janoist1@gmail.com>
2014-06-03 10:32:24 +0200

    Removed unnecessary refresh

--- js/controllers/tableController.js
+++ js/controllers/tableController.js
@@ -2,9 +2,5 @@
 
 damFattyControllers.controller('TableController', ['$scope', 'Game', 'Graphics',
     function ($scope, Game, Graphics) {
-
-        Game.on(Game.EVENT.PLAYER_CHANGE, function () {
-            Graphics.refresh();
-        });
     }
 ]);
